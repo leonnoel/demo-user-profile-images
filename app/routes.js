fa64b73daee34869eb0e36237d3526ae3854801c
Leon Noel <leon.a.noel+github@gmail.com>
2018-04-11 14:15:46 -0400

    current - working

--- app/routes.js
+++ app/routes.js
@@ -24,7 +24,11 @@ module.exports = function(app, passport, db, multer, ObjectId) {
         res.redirect('/');
     });
 
-// message board routes ===============================================================
+//Test =========================================================================
+app.get('/test', isLoggedIn, function(req, res) {
+    console.log(req.session.passport.user)
+});
+// message board routes ========================================================
 
     app.post('/messages', (req, res) => {
       db.collection('messages').save({name: req.body.name, msg: req.body.msg, thumbUp: 0, thumbDown:0}, (err, result) => {
--- config/passport.js
+++ config/passport.js
@@ -59,7 +59,7 @@ module.exports = function(passport) {
                 // set the user's local credentials
                 newUser.local.email    = email;
                 newUser.local.password = newUser.generateHash(password); // use the generateHash function in our user model
-                newUser.profileImg = "placeholder";
+                newUser.profileImg = "images/uploads/rc-logo.png";
 				// save the user
                 newUser.save(function(err) {
                     if (err)
